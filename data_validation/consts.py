0a0376a54147af4c91872876d687f8a1a8607283
Dylan Hercher <dhercher@google.com>
2020-05-23 00:54:06 +0000

    adding new config values decided on and cleaning old keys

--- data_validation/consts.py
+++ data_validation/consts.py
@@ -18,13 +18,12 @@ SOURCE_TYPE = "source_type"
 CONFIG = "config"
 SCHEMA_NAME = "schema_name"
 TABLE_NAME = "table_name"
-PARTITION_COLUMN = "partition_column"
+CONFIG_FIELD_ALIAS = "field_alias"
+CONFIG_GROUPED_COLUMNS = "grouped_columns"
+CONFIG_SOURCE_COLUMN = "source_column"
+CONFIG_TARGET_COLUMN = "target_column"
+CONFIG_CAST = "cast"
 CONFIG_LIMIT = "limit"
-
-# Configurations Used in SQL Generation
-DEFAULT_PARTITION_KEY = "partition_key"
-PARTITION_COLUMN_SQL = "partition_column_sql"
-GROUP_COLUMN_SQL = "grouped_column_sql"
 
 # SQL Template Formatting
 # TODO: should this be managed in query_builder if that is the only place its used?
