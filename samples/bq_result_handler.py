3a24a1662e88f0ddb9b138cb75063515765b52bd
Tim Swast <swast@google.com>
2020-06-02 17:03:39 -0500

    refactor combiner to match BigQuery table format
    For consistency with other parts of the tool, use an ibis pandas client
    for combine step.

---
+++ samples/bq_result_handler.py
@@ -0,0 +1,33 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from data_validation import consts, data_validation
+
+
+BQ_CONN = {"source_type": "BigQuery", "project_id": os.environ["PROJECT_ID"]}
+CONFIG_COUNT_VALID = {
+    # BigQuery Specific Connection Config
+    "source_conn": BQ_CONN,
+    "target_conn": BQ_CONN,
+    # Validation Type
+    "type": "Column",
+    # Configuration Required Depending on Validator Type
+    "schema_name": "bigquery-public-data.new_york_citibike",
+    "table_name": "citibike_trips",
+}
+
+validator = data_validation.DataValidation(CONFIG_COUNT_VALID, verbose=True)
+df = validator.execute()
