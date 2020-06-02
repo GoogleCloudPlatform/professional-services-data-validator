00dda89b37c9b6714143dfcc6cd0fcbe5082a2e1
Dylan Hercher <dylanhercher@gmail.com>
2020-06-02 13:25:15 -0700

    Update tests/unit/test_validation_builder.py
    Co-authored-by: Tim Swast <swast@google.com>

--- tests/unit/test_validation_builder.py
+++ tests/unit/test_validation_builder.py
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from copy import deepcopy
+
 import pytest
 
-from copy import deepcopy
 from data_validation import consts
 
 
