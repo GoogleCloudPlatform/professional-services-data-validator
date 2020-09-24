fce97b722436a440154c7f98e30491ee4b13f43b
Dylan Hercher <dhercher@google.com>
2020-09-24 15:18:05 +0000

    lint

--- data_validation/data_validation.py
+++ data_validation/data_validation.py
@@ -89,7 +89,7 @@ class DataValidation(object):
 
     def execute_recursive_validation(self, validation_builder, grouped_fields):
         """ Recursive execution for Row validations.
-        
+
         This method executes aggregate queries, such as sum-of-hashes, on the
         source and target tables. Where they differ, add to the GROUP BY
         clause recursively until the individual row differences can be
