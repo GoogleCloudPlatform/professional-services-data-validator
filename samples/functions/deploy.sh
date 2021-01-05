7639c152e13ebc3e8e592e17524090b7c9bf643a
dhercher <dhercher@google.com>
2021-01-05 02:19:44 +0000

    Working deploy to cloud functions

--- samples/functions/deploy.sh
+++ samples/functions/deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+
 export PROJECT_ID=pso-kokoro-resources
 export BUCKET=pso-kokoro-resources
 export REGION=us-central1
 
+cd samples/functions/
 
-cp ../requirements.txt
+cp ../../requirements.txt .
+echo "# Install Data Validation" >> requirements.txt
+echo "https://storage.googleapis.com/professional-services-data-validator/releases/latest/google_pso_data_validator-latest-py3-none-any.whl" >> requirements.txt
+
 zip -r data_validation.zip .
 gsutil cp data_validation.zip gs://${BUCKET}/
-
-gsutil iam ch serviceAccount:service-38376331460@gcf-admin-robot.iam.gserviceaccount.com:objectAdmin gs://artifacts.${PROJECT_ID}.appspot.com/
-gsutil iam ch serviceAccount:service-38376331460@gcf-admin-robot.iam.gserviceaccount.com:objectAdmin gs://us.artifacts.${PROJECT_ID}.appspot.com/
 
 gcloud functions deploy data-validation --region=${REGION} \
 	--entry-point=main \
@@ -20,6 +22,8 @@ gcloud functions deploy data-validation --region=${REGION} \
 	--project=${PROJECT_ID}
 
 rm data_validation.zip
+rm requirements.txt
+cd ../../
 
 # Run Test
 export TEST_DATA='{"cmd":"ls"}'
