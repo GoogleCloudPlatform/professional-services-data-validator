3c6274fcb294a9e03f743d1494c88ca916621114
Mike Hilton <emceehilton@google.com>
2021-01-06 01:37:46 +0000

    adding test coverage

--- tests/unit/test_validation_builder.py
+++ tests/unit/test_validation_builder.py
@@ -64,9 +64,25 @@ AGGREGATES_TEST = [
 CALCULATED_MULTIPLE_TEST = [
     {
         consts.CONFIG_FIELD_ALIAS: "concat_start_station_name_end_station_name",
-        consts.CONFIG_SOURCE_CALCULATED_COLUMNS: ["start_station_name", "end_station_name"],
-        consts.CONFIG_TARGET_CALCULATED_COLUMNS: ["start_station_name", "end_station_name"],
+        consts.CONFIG_SOURCE_CALCULATED_COLUMNS: ["start_station_name"],
+        consts.CONFIG_TARGET_CALCULATED_COLUMNS: ["start_station_name"],
         consts.CONFIG_TYPE: "concat",
+        consts.CONFIG_FIELD_ALIAS: "length_start_station_name",
+        consts.CONFIG_SOURCE_CALCULATED_COLUMNS: ["start_station_name"],
+        consts.CONFIG_TARGET_CALCULATED_COLUMNS: ["start_station_name"],
+        consts.CONFIG_TYPE: "length",
+        consts.CONFIG_FIELD_ALIAS: "ifnull_start_station_name",
+        consts.CONFIG_SOURCE_CALCULATED_COLUMNS: ["start_station_name"],
+        consts.CONFIG_TARGET_CALCULATED_COLUMNS: ["start_station_name"],
+        consts.CONFIG_TYPE: "ifnull",
+        consts.CONFIG_FIELD_ALIAS: "rstrip_start_station_name",
+        consts.CONFIG_SOURCE_CALCULATED_COLUMNS: ["start_station_name"],
+        consts.CONFIG_TARGET_CALCULATED_COLUMNS: ["start_station_name"],
+        consts.CONFIG_TYPE: "rstrip",
+        consts.CONFIG_FIELD_ALIAS: "upper_start_station_name",
+        consts.CONFIG_SOURCE_CALCULATED_COLUMNS: ["start_station_name"],
+        consts.CONFIG_TARGET_CALCULATED_COLUMNS: ["start_station_name"],
+        consts.CONFIG_TYPE: "upper",
     },
 ]
 
@@ -127,7 +143,12 @@ def test_column_validation_calculate(module_under_test):
     mock_config_manager.append_calculated_fields(CALCULATED_MULTIPLE_TEST)
     builder.add_config_calculated_fields()
 
-    assert list(builder.get_metadata().keys()) == ["concat_start_station_name_end_station_name"]
+    assert list(builder.get_metadata().keys()) == ["concat_start_station_name_end_station_name",
+                                                   "length_start_station_name",
+                                                   "ifnull_start_station_name",
+                                                   "rstrip_start_station_name",
+                                                   "upper_start_station_name",
+                                                  ]
 
 
 def test_column_validation_limit(module_under_test):
