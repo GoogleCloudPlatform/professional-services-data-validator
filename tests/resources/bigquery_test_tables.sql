f20d0dde25f985fca08c83a23e1d0748884197cf
dhercher <dhercher@google.com>
2021-03-19 02:01:35 +0000

    adding a timestamp to test

--- tests/resources/bigquery_test_tables.sql
+++ tests/resources/bigquery_test_tables.sql
@@ -5,4 +5,6 @@ SELECT
     CAST('1234567890123456789012345678901234.00' AS BIGNUMERIC) bignumeric_type,
     CAST(2 AS INT64) int_type,
     CAST(2 AS DECIMAL) decimal_type,
-    CAST(2 AS STRING) text_type
+    CAST(2 AS STRING) text_type,
+    CAST('2021-01-01 00:00:00' AS TIMESTAMP) timestamp_type
+
