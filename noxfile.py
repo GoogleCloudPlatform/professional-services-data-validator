7b486c1fa30078789976472ddcce155fe9ff8888
Neha Nene <nehanene@google.com>
2021-04-14 12:32:52 -0500

    typo in noxfile

--- noxfile.py
+++ noxfile.py
@@ -115,7 +115,7 @@ def integration_mysql(session):
     _setup_session_requirements(session, extra_packages=[])
 
     test_path = "tests/system/data_sources/test_mysql.py"
-    expected_env_vars = ["MYSQL_HOST", "MYSQL_PASSWORD", "CLOUD_SQL_CONNECTION"]
+    expected_env_vars = ["MYSQL_HOST", "MYSQL_PASSWORD"]
     for env_var in expected_env_vars:
         if not os.environ.get(env_var, ""):
             raise Exception("Expected Env Var: %s" % env_var)
@@ -151,7 +151,7 @@ def integration_sql_server(session):
     _setup_session_requirements(session, extra_packages=["pyodbc"])
 
     test_path = "tests/system/data_sources/test_sql_server.py"
-    expected_env_vars = ["PROJECT_ID", "SQL_SERVER_PASSWORD"]
+    expected_env_vars = ["PROJECT_ID", "SQL_SERVER_PASSWORD", "CLOUD_SQL_CONNECTION"]
     for env_var in expected_env_vars:
         if not os.environ.get(env_var, ""):
             raise Exception("Expected Env Var: %s" % env_var)
