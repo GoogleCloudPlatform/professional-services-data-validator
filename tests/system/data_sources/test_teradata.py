edbebd5fd0b906f91f29d5d8d7a68f92980c5ddc
Mike Hilton <emceehilton@google.com>
2021-11-01 21:08:28 +0000

    linting

--- tests/system/data_sources/test_teradata.py
+++ tests/system/data_sources/test_teradata.py
@@ -12,11 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from datetime import datetime
 import os
 
 from data_validation import data_validation, consts
-from data_validation.query_builder import query_builder
 
 TERADATA_PASSWORD = os.getenv("TERADATA_PASSWORD")
 TERADATA_HOST = os.getenv("TERADATA_HOST")
@@ -66,16 +64,3 @@ def test_count_validator():
     df = validator.execute()
     assert int(df["source_agg_value"][0]) > 0
     assert df["source_agg_value"][0] == df["target_agg_value"][0]
-
-
-def _add_calendar_date_filters(builder):
-    # Adding custom filter for better results
-    project_start_date = datetime(2020, 1, 1, 0, 0, 0)
-    filter_obj = query_builder.FilterField.less_than(
-        "calendar_date", project_start_date
-    )
-    builder.add_filter_field(filter_obj)
-
-    in_the_past = datetime(1991, 5, 2, 0, 0, 0)
-    filter_obj = query_builder.FilterField.greater_than("calendar_date", in_the_past)
-    builder.add_filter_field(filter_obj)
