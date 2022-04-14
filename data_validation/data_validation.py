9169fc4d490c83e658e54d1912cdf6341dcf65a4
Robby <robbysingh@google.com>
2022-04-14 14:28:54 +0530

    Fixed custom query column bug

--- data_validation/data_validation.py
+++ data_validation/data_validation.py
@@ -291,13 +291,19 @@ class DataValidation(object):
             if self.config_manager.validation_type == consts.ROW_VALIDATION
             else set(validation_builder.get_group_aliases())
         )
-        if self.config_manager.validation_type == consts.CUSTOM_QUERY:
+        if (
+            self.config_manager.validation_type == consts.CUSTOM_QUERY
+            and "hash__all" in self.run_metadata.validations
+        ):
             join_on_fields = set(["hash__all"])
 
         # If row validation from YAML, compare source and target agg values
         is_value_comparison = (
             self.config_manager.validation_type == consts.ROW_VALIDATION
-            or self.config_manager.validation_type == consts.CUSTOM_QUERY
+            or (
+                self.config_manager.validation_type == consts.CUSTOM_QUERY
+                and "hash__all" in self.run_metadata.validations
+            )
         )
 
         if process_in_memory:
