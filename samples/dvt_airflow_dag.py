5c3cf428ffca3f69c006c3a547bb2c2cbac0c14d
Helen Cristina Ferreira <helencristina@google.com>
2022-05-23 20:36:36 +0000

    feat: Change DAG for simple count. Remove old DAG

--- samples/dvt_airflow_dag.py
+++ samples/dvt_airflow_dag.py
@@ -15,24 +15,18 @@
 """
 
 """
-DAG for Data Validator Tool
+Example DAG for Data Validation Tool
 
-https://github.com/GoogleCloudPlatform/professional-services-data-validator
-
-Requirements
-- Composer created with Public IP (Private environment disabled)
-
-Steps before run DAG
-- Create Airflow variable PROJECT_ID with the GCP Project ID
-- Create BigQuery dataset and table:
-
-bq mk pso_data_validator
-wget https://raw.githubusercontent.com/GoogleCloudPlatform/professional-services-data-validator/develop/terraform/results_schema.json
-bq mk --table \
-  --time_partitioning_field start_time \
-  --clustering_fields validation_name,run_id \
-  pso_data_validator.results \
-  results_schema.json
+Requirements to run the DAG:
+- Airflow environment created with Public IP (Private environment disabled)
+- Create an Airflow variable called 'gcp_project' with the GCP Project ID
+- Create BigQuery dataset and table to store the validation results
+  bq mk pso_data_validator
+  bq mk --table \
+    --time_partitioning_field start_time \
+    --clustering_fields validation_name,run_id \
+    pso_data_validator.results \
+    results_schema.json
 """
 
 from datetime import timedelta
@@ -40,7 +34,7 @@ from datetime import timedelta
 import airflow
 from airflow import DAG
 from airflow import models
-from airflow.operators.python_operator import PythonVirtualenvOperator
+from airflow.operators.python import PythonVirtualenvOperator
 
 default_args = {
     "start_date": airflow.utils.dates.days_ago(1),
@@ -56,11 +50,11 @@ with DAG(
     dagrun_timeout=timedelta(minutes=60),
 ) as dag:
 
-  def data_validation_function(project_id):
+  def validation_function(project):
     from data_validation import data_validation
     from data_validation.result_handlers import bigquery as bqhandler
 
-    BQ_CONN = {"source_type": "BigQuery", "project_id": project_id}
+    BQ_CONN = {"source_type": "BigQuery", "project_id": project}
 
     GROUPED_CONFIG_COUNT_VALID = {
         # BigQuery Specific Connection Config
@@ -71,17 +65,10 @@ with DAG(
         # Configuration Required Depending on Validator Type
         "schema_name": "bigquery-public-data.new_york_citibike",
         "table_name": "citibike_trips",
-        "grouped_columns": [
-            {
-                "field_alias": "starttime",
-                "source_column": "starttime",
-                "target_column": "starttime",
-                "cast": "date",
-            },
-        ],
         "aggregates": [
             {
                 "field_alias": "count",
+                # Tool will run a 'COUNT *' as the default aggregation
                 "source_column": None,
                 "target_column": None,
                 "type": "count",
@@ -89,18 +76,18 @@ with DAG(
         ],
     }
 
-    result_handler = bqhandler.BigQueryResultHandler.get_handler_for_project(project_id)
+    handler = bqhandler.BigQueryResultHandler.get_handler_for_project(project)
     validator = data_validation.DataValidation(
-        GROUPED_CONFIG_COUNT_VALID, verbose=True, result_handler=result_handler
+        GROUPED_CONFIG_COUNT_VALID, verbose=True, result_handler=handler
     )
-    df = validator.execute()
+    validator.execute()
 
-  PROJECT_ID = models.Variable.get('PROJECT_ID')
+  gcp_project = models.Variable.get('gcp_project')
 
   virtualenv_task = PythonVirtualenvOperator(
       task_id="dvt-virtualenv",
-      python_callable=data_validation_function,
-      op_args=[PROJECT_ID],
+      python_callable=validation_function,
+      op_args=[gcp_project],
       requirements=["google-pso-data-validator"],
       system_site_packages=False,
   )
