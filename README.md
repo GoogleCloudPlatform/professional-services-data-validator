e85d09b6a09a10c613b8c49513060ff5c7678b05
Helen Cristina Ferreira <helencristina@google.com>
2022-05-24 15:21:37 +0000

    docs: Add section for Airflow on Installation page

--- README.md
+++ README.md
@@ -638,36 +638,7 @@ in the Data Validation tool, it is a simple process.
 3.  You are done, you can reference the data source via the config.
 
     -   Config: {"source_type": "<RefName>", ...KV Values required in Client...}
-
-## Deploy to Composer
-
-```
-#!/bin/bash
-
-export COMPOSER_ENV=""
-export LOCATION=""
-
-echo "Creating Composer Env: $COMPOSER_ENV"
-gcloud services enable composer.googleapis.com
-gcloud composer environments create $COMPOSER_ENV --location=$LOCATION --python-version=3
-
-echo "Updating Composer Env Reqs: $COMPOSER_ENV"
-# Composer builds Pandas and BigQuery for you, these should be stripped out
-cat requirements.txt | grep -v pandas | grep -v google-cloud-bigquery > temp_reqs.txt
-gcloud composer environments update $COMPOSER_ENV --location=$LOCATION --update-pypi-packages-from-file=temp_reqs.txt
-rm temp_reqs.txt
-
-# Deploy Package to Composer (the hacky way)
-echo "Rebuilding Data Validation Package in GCS"
-export GCS_BUCKET_PATH=$(gcloud composer environments describe $COMPOSER_ENV --location=$LOCATION | grep dagGcsPrefix | awk '{print $2;}')
-gsutil rm -r $GCS_BUCKET_PATH/data_validation
-gsutil cp -r data_validation $GCS_BUCKET_PATH/data_validation
-
-# Deploy Test DAG to Composer
-echo "Pushing Data Validation Test Operator to GCS"
-gsutil cp tests/test_data_validation_operators.py $GCS_BUCKET_PATH/
-```
-
+    
 ## Contributing
 
 Contributions are welcome. See the [contributing guide](CONTRIBUTING.md) for
--- samples/airflow/dvt_airflow_dag.py
+++ samples/airflow/dvt_airflow_dag.py
@@ -50,46 +50,46 @@ with DAG(
     dagrun_timeout=timedelta(minutes=60),
 ) as dag:
 
-  def validation_function(project):
-    from data_validation import data_validation
-    from data_validation.result_handlers import bigquery as bqhandler
+    def validation_function(project):
+        from data_validation import data_validation
+        from data_validation.result_handlers import bigquery as bqhandler
 
-    BQ_CONN = {"source_type": "BigQuery", "project_id": project}
+        BQ_CONN = {"source_type": "BigQuery", "project_id": project}
 
-    GROUPED_CONFIG_COUNT_VALID = {
-        # BigQuery Specific Connection Config
-        "source_conn": BQ_CONN,
-        "target_conn": BQ_CONN,
-        # Validation Type
-        "type": "Column",
-        # Configuration Required Depending on Validator Type
-        "schema_name": "bigquery-public-data.new_york_citibike",
-        "table_name": "citibike_trips",
-        "aggregates": [
-            {
-                "field_alias": "count",
-                # Tool will run a 'COUNT *' as the default aggregation
-                "source_column": None,
-                "target_column": None,
-                "type": "count",
-            },
-        ],
-    }
+        GROUPED_CONFIG_COUNT_VALID = {
+            # BigQuery Specific Connection Config
+            "source_conn": BQ_CONN,
+            "target_conn": BQ_CONN,
+            # Validation Type
+            "type": "Column",
+            # Configuration Required Depending on Validator Type
+            "schema_name": "bigquery-public-data.new_york_citibike",
+            "table_name": "citibike_trips",
+            "aggregates": [
+                {
+                    "field_alias": "count",
+                    # Tool will run a 'COUNT *' as the default aggregation
+                    "source_column": None,
+                    "target_column": None,
+                    "type": "count",
+                },
+            ],
+        }
 
-    handler = bqhandler.BigQueryResultHandler.get_handler_for_project(project)
-    validator = data_validation.DataValidation(
-        GROUPED_CONFIG_COUNT_VALID, verbose=True, result_handler=handler
+        handler = bqhandler.BigQueryResultHandler.get_handler_for_project(project)
+        validator = data_validation.DataValidation(
+            GROUPED_CONFIG_COUNT_VALID, verbose=True, result_handler=handler
+        )
+        validator.execute()
+
+    gcp_project = models.Variable.get("gcp_project")
+
+    virtualenv_task = PythonVirtualenvOperator(
+        task_id="dvt-virtualenv",
+        python_callable=validation_function,
+        op_args=[gcp_project],
+        requirements=["google-pso-data-validator"],
+        system_site_packages=False,
     )
-    validator.execute()
-
-  gcp_project = models.Variable.get('gcp_project')
-
-  virtualenv_task = PythonVirtualenvOperator(
-      task_id="dvt-virtualenv",
-      python_callable=validation_function,
-      op_args=[gcp_project],
-      requirements=["google-pso-data-validator"],
-      system_site_packages=False,
-  )
 
 virtualenv_task
