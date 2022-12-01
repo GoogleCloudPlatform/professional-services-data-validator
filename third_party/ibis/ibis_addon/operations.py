121e71b738f7fc4786a99210dc22bf32725d2e1c
Sundar Mudupalli <106282841+sundar-mudupalli-work@users.noreply.github.com>
2022-12-01 14:36:36 -0800

    Updating to support TO_CHAR for Temporal types - date, Time and Timestamp for now

--- third_party/ibis/ibis_addon/operations.py
+++ third_party/ibis/ibis_addon/operations.py
@@ -33,7 +33,7 @@ import ibis.expr.rules as rlz
 from data_validation.clients import _raise_missing_client_error
 from ibis_bigquery.compiler import reduction as bq_reduction, BigQueryExprTranslator
 from ibis.expr.operations import Arg, Comparison, Reduction, ValueOp
-from ibis.expr.types import BinaryValue, IntegerColumn, StringValue, NumericValue
+from ibis.expr.types import BinaryValue, IntegerColumn, StringValue, NumericValue, TemporalValue
 from ibis.backends.impala.compiler import ImpalaExprTranslator
 from ibis.backends.pandas import client as _pandas_client
 from ibis.backends.base_sqlalchemy.alchemy import AlchemyExprTranslator
@@ -67,7 +67,7 @@ class HashBytes(ValueOp):
     output_type = rlz.shape_like("arg", "binary")
 
 class ToChar(ValueOp):
-    arg = Arg(rlz.one_of([rlz.value(dt.Decimal), rlz.value(dt.float64)]))
+    arg = Arg(rlz.one_of([rlz.value(dt.Decimal), rlz.value(dt.float64), rlz.value(dt.Date), rlz.value(dt.Time), rlz.value(dt.Timestamp)]))
     fmt = Arg(rlz.string)
     output_type = rlz.shape_like("arg", dt.string)
 
