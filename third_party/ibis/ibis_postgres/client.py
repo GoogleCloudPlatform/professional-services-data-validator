35011e9bb2dae12330c6037de9792698c2354add
sharangagarwal <sharangagarwal@google.com>
2023-02-15 12:21:59 +0530

    fix: replace := opertor with python 3.7 compatible assignment

--- third_party/ibis/ibis_postgres/client.py
+++ third_party/ibis/ibis_postgres/client.py
@@ -21,9 +21,11 @@ import ast
 import toolz
 import ibis.expr.datatypes as dt
 import ibis.expr.schema as sch
+
 # import ibis.expr.types as ir
 from ibis import util
 from ibis.backends.postgres.client import PostgreSQLClient
+
 
 def _get_schema_using_query(self, query: str) -> sch.Schema:
     raw_name = util.guid()
@@ -47,82 +49,24 @@ ORDER BY attnum
     tuples = [(col, self._get_type(typestr)) for col, typestr in type_info]
     return sch.Schema.from_tuples(tuples)
 
-# def _get_type(self,typestr: str) -> dt.DataType:
-#     _type_mapping = {
-#     # "boolean": dt.bool,
-#     "boolean": dt.boolean,
-#     "boolean[]": dt.Array(dt.boolean),
-#     "bytea": dt.binary,
-#     "bytea[]": dt.Array(dt.binary),
-#     "character(1)": dt.string,
-#     "character(1)[]": dt.Array(dt.string),
-#     "bigint": dt.int64,
-#     "bigint[]": dt.Array(dt.int64),
-#     "smallint": dt.int16,
-#     "smallint[]": dt.Array(dt.int16),
-#     "integer": dt.int32,
-#     "integer[]": dt.Array(dt.int32),
-#     "text": dt.string,
-#     "text[]": dt.Array(dt.string),
-#     "json": dt.json,
-#     "json[]": dt.Array(dt.json),
-#     "point": dt.point,
-#     "point[]": dt.Array(dt.point),
-#     "polygon": dt.polygon,
-#     "polygon[]": dt.Array(dt.polygon),
-#     "line": dt.linestring,
-#     "line[]": dt.Array(dt.linestring),
-#     "real": dt.float32,
-#     "real[]": dt.Array(dt.float32),
-#     "double precision": dt.float64,
-#     "double precision[]": dt.Array(dt.float64),
-#     "macaddr8": dt.macaddr,
-#     "macaddr8[]": dt.Array(dt.macaddr),
-#     "macaddr": dt.macaddr,
-#     "macaddr[]": dt.Array(dt.macaddr),
-#     "inet": dt.inet,
-#     "inet[]": dt.Array(dt.inet),
-#     "character": dt.string,
-#     "character[]": dt.Array(dt.string),
-#     "character varying": dt.string,
-#     "character varying[]": dt.Array(dt.string),
-#     "date": dt.date,
-#     "date[]": dt.Array(dt.date),
-#     "time without time zone": dt.time,
-#     "time without time zone[]": dt.Array(dt.time),
-#     "timestamp without time zone": dt.timestamp,
-#     "timestamp without time zone[]": dt.Array(dt.timestamp),
-#     "timestamp with time zone": dt.Timestamp("UTC"),
-#     "timestamp with time zone[]": dt.Array(dt.Timestamp("UTC")),
-#     "interval": dt.interval,
-#     "interval[]": dt.Array(dt.interval),
-#     # NB: this isn"t correct, but we try not to fail
-#     "time with time zone": "time",
-#     "numeric": dt.Decimal,
-#     "numeric[]": dt.Array(dt.Decimal),
-#     "uuid": dt.uuid,
-#     "uuid[]": dt.Array(dt.uuid),
-#     "jsonb": dt.jsonb,
-#     "jsonb[]": dt.Array(dt.jsonb),
-#     "geometry": dt.geometry,
-#     "geometry[]": dt.Array(dt.geometry),
-#     "geography": dt.geography,
-#     "geography[]": dt.Array(dt.geography),
-#     }
+
 _BRACKETS = "[]"
 _STRING_REGEX = (
     """('[^\n'\\\\]*(?:\\\\.[^\n'\\\\]*)*'|"[^\n"\\\\"]*(?:\\\\.[^\n"\\\\]*)*")"""
 )
 
+
 def spaceless(parser):
     return SPACES.then(parser).skip(SPACES)
 
+
 def spaceless_string(*strings: str):
     return spaceless(
-        parsy.alt(*(parsy.string(string, transform=str.lower) for string in strings))
+        parsy.alt(*(parsy.string(s, transform=str.lower) for s in strings))
     )
 
-SPACES = parsy.regex(r'\s*', re.MULTILINE)
+
+SPACES = parsy.regex(r"\s*", re.MULTILINE)
 RAW_NUMBER = parsy.decimal_digit.at_least(1).concat()
 SINGLE_DIGIT = parsy.decimal_digit
 PRECISION = SCALE = NUMBER = RAW_NUMBER.map(int)
@@ -143,12 +87,13 @@ SEMICOLON = spaceless_string(";")
 RAW_STRING = parsy.regex(_STRING_REGEX).map(ast.literal_eval)
 FIELD = parsy.regex("[a-zA-Z_][a-zA-Z_0-9]*")
 
+
 def _parse_numeric(
-    text: str, default_decimal_parameters: tuple[int | None, int | None] = (None, None)
+    text: str, ddp: tuple[int | None, int | None] = (None, None)
 ) -> dt.DataType:
     decimal = spaceless_string("decimal", "numeric").then(
         parsy.seq(LPAREN.then(PRECISION.skip(COMMA)), SCALE.skip(RPAREN))
-        .optional(default_decimal_parameters)
+        .optional(ddp)
         .combine(dt.Decimal)
     )
 
@@ -161,9 +106,11 @@ def _parse_numeric(
     ty = pg_array | decimal
     return ty.parse(text)
 
-def _get_type(self,typestr: str) -> dt.DataType:
+
+def _get_type(self, typestr: str) -> dt.DataType:
     is_array = typestr.endswith(_BRACKETS)
-    if (typ := _type_mapping.get(typestr.replace(_BRACKETS, ""))) is not None:
+    typ = _type_mapping.get(typestr.replace(_BRACKETS, ""))
+    if typ is not None:
         return dt.Array(typ) if is_array else typ
     return _parse_numeric(typestr)
 
