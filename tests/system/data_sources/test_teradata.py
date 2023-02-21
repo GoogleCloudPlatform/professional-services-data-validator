6e5a29fe878914dc83791b790dd5b3b440e35046
nj1973 <neil@thirdchimp.net>
2023-02-21 18:32:01 +0000

    test: Add integration test for DATE Teradata column

--- tests/system/data_sources/test_teradata.py
+++ tests/system/data_sources/test_teradata.py
@@ -30,7 +30,7 @@ conn = {
 }
 
 
-TERADATA_CONFIG = {
+TERADATA_COLUMN_CONFIG = {
     # Specific Connection Config
     consts.CONFIG_SOURCE_CONN: conn,
     consts.CONFIG_TARGET_CONN: conn,
@@ -60,9 +60,130 @@ TERADATA_CONFIG = {
     ],
 }
 
+TERADATA_ROW_CONFIG = {
+    # Specific Connection Config
+    consts.CONFIG_SOURCE_CONN: conn,
+    consts.CONFIG_TARGET_CONN: conn,
+    # Validation Type
+    consts.CONFIG_TYPE: "Row",
+    # Configuration Required Depending on Validator Type
+    consts.CONFIG_SCHEMA_NAME: "Sys_Calendar",
+    consts.CONFIG_TABLE_NAME: "CALENDAR",
+    consts.CONFIG_TARGET_SCHEMA_NAME: "Sys_Calendar",
+    consts.CONFIG_TARGET_TABLE_NAME: "CALENDAR",
+    consts.CONFIG_THRESHOLD: 0.0,
+    consts.CONFIG_FORMAT: "table",
+    consts.CONFIG_FILTER_STATUS: None,
+    consts.CONFIG_FILTERS: [],
+    consts.CONFIG_USE_RANDOM_ROWS: False,
+    consts.CONFIG_COMPARISON_FIELDS: [
+        {
+            consts.CONFIG_SOURCE_COLUMN: "concat__all",
+            consts.CONFIG_TARGET_COLUMN: "concat__all",
+            consts.CONFIG_FIELD_ALIAS: "concat__all",
+            consts.CONFIG_CAST: None,
+        }
+    ],
+    consts.CONFIG_CALCULATED_FIELDS: [
+        {
+            consts.CONFIG_CALCULATED_SOURCE_COLUMNS: ["calendar_date"],
+            consts.CONFIG_CALCULATED_TARGET_COLUMNS: ["calendar_date"],
+            consts.CONFIG_FIELD_ALIAS: "cast__calendar_date",
+            consts.CONFIG_TYPE: "custom",
+            consts.CONFIG_DEPTH: 0,
+            consts.CONFIG_CUSTOM_IBIS_EXPR: "ibis.expr.api.TimestampValue.strftime",
+            consts.CONFIG_CUSTOM_PARAMS: [{"format_str": "%Y-%m-%d"}],
+        },
+        {
+            consts.CONFIG_CALCULATED_SOURCE_COLUMNS: ["day_of_week"],
+            consts.CONFIG_CALCULATED_TARGET_COLUMNS: ["day_of_week"],
+            consts.CONFIG_FIELD_ALIAS: "cast__day_of_week",
+            consts.CONFIG_TYPE: "cast",
+            consts.CONFIG_DEPTH: 0,
+        },
+        {
+            consts.CONFIG_CALCULATED_SOURCE_COLUMNS: ["cast__calendar_date"],
+            consts.CONFIG_CALCULATED_TARGET_COLUMNS: ["cast__calendar_date"],
+            consts.CONFIG_FIELD_ALIAS: "ifnull__cast__calendar_date",
+            consts.CONFIG_TYPE: "ifnull",
+            consts.CONFIG_DEPTH: 1,
+        },
+        {
+            consts.CONFIG_CALCULATED_SOURCE_COLUMNS: ["cast__day_of_week"],
+            consts.CONFIG_CALCULATED_TARGET_COLUMNS: ["cast__day_of_week"],
+            consts.CONFIG_FIELD_ALIAS: "ifnull__cast__day_of_week",
+            consts.CONFIG_TYPE: "ifnull",
+            consts.CONFIG_DEPTH: 1,
+        },
+        {
+            consts.CONFIG_CALCULATED_SOURCE_COLUMNS: ["ifnull__cast__calendar_date"],
+            consts.CONFIG_CALCULATED_TARGET_COLUMNS: ["ifnull__cast__calendar_date"],
+            consts.CONFIG_FIELD_ALIAS: "rstrip__ifnull__cast__calendar_date",
+            consts.CONFIG_TYPE: "rstrip",
+            consts.CONFIG_DEPTH: 2,
+        },
+        {
+            consts.CONFIG_CALCULATED_SOURCE_COLUMNS: ["ifnull__cast__day_of_week"],
+            consts.CONFIG_CALCULATED_TARGET_COLUMNS: ["ifnull__cast__day_of_week"],
+            consts.CONFIG_FIELD_ALIAS: "rstrip__ifnull__cast__day_of_week",
+            consts.CONFIG_TYPE: "rstrip",
+            consts.CONFIG_DEPTH: 2,
+        },
+        {
+            consts.CONFIG_CALCULATED_SOURCE_COLUMNS: [
+                "rstrip__ifnull__cast__calendar_date"
+            ],
+            consts.CONFIG_CALCULATED_TARGET_COLUMNS: [
+                "rstrip__ifnull__cast__calendar_date"
+            ],
+            consts.CONFIG_FIELD_ALIAS: "upper__rstrip__ifnull__cast__calendar_date",
+            consts.CONFIG_TYPE: "upper",
+            consts.CONFIG_DEPTH: 3,
+        },
+        {
+            consts.CONFIG_CALCULATED_SOURCE_COLUMNS: [
+                "rstrip__ifnull__cast__day_of_week"
+            ],
+            consts.CONFIG_CALCULATED_TARGET_COLUMNS: [
+                "rstrip__ifnull__cast__day_of_week"
+            ],
+            consts.CONFIG_FIELD_ALIAS: "upper__rstrip__ifnull__cast__day_of_week",
+            consts.CONFIG_TYPE: "upper",
+            consts.CONFIG_DEPTH: 3,
+        },
+        {
+            consts.CONFIG_CALCULATED_SOURCE_COLUMNS: [
+                "upper__rstrip__ifnull__cast__calendar_date",
+                "upper__rstrip__ifnull__cast__day_of_week",
+            ],
+            consts.CONFIG_CALCULATED_TARGET_COLUMNS: [
+                "upper__rstrip__ifnull__cast__calendar_date",
+                "upper__rstrip__ifnull__cast__day_of_week",
+            ],
+            consts.CONFIG_FIELD_ALIAS: "concat__all",
+            consts.CONFIG_TYPE: "concat",
+            consts.CONFIG_DEPTH: 4,
+        },
+    ],
+    consts.CONFIG_PRIMARY_KEYS: [
+        {
+            consts.CONFIG_SOURCE_COLUMN: "calendar_date",
+            consts.CONFIG_TARGET_COLUMN: "calendar_date",
+            consts.CONFIG_FIELD_ALIAS: "calendar_date",
+            consts.CONFIG_CAST: None,
+        }
+    ],
+}
+
 
 def test_count_validator():
-    validator = data_validation.DataValidation(TERADATA_CONFIG, verbose=True)
+    validator = data_validation.DataValidation(TERADATA_COLUMN_CONFIG, verbose=True)
     df = validator.execute()
     assert int(df["source_agg_value"][0]) > 0
     assert df["source_agg_value"][0] == df["target_agg_value"][0]
+
+
+def test_row_validator():
+    validator = data_validation.DataValidation(TERADATA_ROW_CONFIG, verbose=True)
+    df = validator.execute()
+    assert df["validation_status"][0] == "success"
