ddc4f4819013a339e13fc836705f47f509ac6754
Moukhtar Ahmed <moukhtar@google.com>
2023-03-27 21:08:03 +0200

    fix: remove secret manager warning

--- data_validation/secret_manager.py
+++ data_validation/secret_manager.py
@@ -7,8 +7,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-
 
 class SecretManagerBuilder:
     def build(self, client_type):
