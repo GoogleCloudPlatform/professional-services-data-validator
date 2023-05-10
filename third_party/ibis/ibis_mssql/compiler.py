c266d21430906d4cea93d3f51ac8e12b0b604977
helensilva14 <helencristina@google.com>
2023-05-10 21:44:23 +0000

    Add missing dtype register for MONEY type for SQL Server

--- third_party/ibis/ibis_mssql/compiler.py
+++ third_party/ibis/ibis_mssql/compiler.py
@@ -18,8 +18,6 @@ import math
 import sqlalchemy as sa
 import sqlalchemy.dialects.mssql as mssql
 
-from sqlalchemy.dialects.mssql.base import MSDialect
-
 import ibis.common.exceptions as com
 import ibis.expr.datatypes as dt
 import ibis.expr.operations as ops
@@ -503,7 +501,6 @@ class MSSQLExprTranslator(alch.AlchemyExprTranslator):
             dt.Int64: mssql.MONEY,
         }
     )
-    #print("~~~ _TYPE_MAP: " + str(_type_map))
 
 
 rewrites = MSSQLExprTranslator.rewrites
