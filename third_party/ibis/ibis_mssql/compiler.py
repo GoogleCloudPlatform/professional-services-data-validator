9238473ceb44a18eb3f834f6f8a8f189884577f0
helensilva14 <helencristina@google.com>
2023-05-10 21:11:07 +0000

    Changed dialect used by the SQL Server compiler

--- third_party/ibis/ibis_mssql/compiler.py
+++ third_party/ibis/ibis_mssql/compiler.py
@@ -18,6 +18,8 @@ import math
 import sqlalchemy as sa
 import sqlalchemy.dialects.mssql as mssql
 
+from sqlalchemy.dialects.mssql.base import MSDialect
+
 import ibis.common.exceptions as com
 import ibis.expr.datatypes as dt
 import ibis.expr.operations as ops
@@ -501,14 +503,16 @@ class MSSQLExprTranslator(alch.AlchemyExprTranslator):
             dt.Int64: mssql.MONEY,
         }
     )
+    #print("~~~ _TYPE_MAP: " + str(_type_map))
 
 
 rewrites = MSSQLExprTranslator.rewrites
 compiles = MSSQLExprTranslator.compiles
 
+
 class MSSQLDialect(alch.AlchemyDialect):
 
-     translator = MSSQLExprTranslator
+      translator = MSSQLExprTranslator
 
 
 dialect = MSSQLDialect
