7783beb347de2a4342810b50c4e65df873351b31
nj1973 <neil@thirdchimp.net>
2023-06-19 22:14:38 +0100

    feat: Support allow list decimals having a range for precision and scale. Also add --allow-list-file. (#888)
    * refactor: Change parse_allow_list() to use regex and add unit tests
    
    * feat: Support allow list decimals having a range for precision and scale
    
    * feat: Support allow list decimals having a range for precision and scale. Also add --allow-list-file.
    
    * feat: Support allow list decimals having a range for precision and scale. Also add --allow-list-file.
    
    * feat: Support allow list decimals having a range for precision and scale. Also add --allow-list-file.
    
    * chore: Change type hints to be valid with older Python versions
    
    * doc: Add docstr to convert_pairs_to_dict()
    
    * chore: Remove unused import

--- data_validation/cli_tools.py
+++ data_validation/cli_tools.py
@@ -336,7 +336,7 @@ def _configure_validation_config_parser(subparsers):
     run_parser.add_argument(
         "--config-file",
         "-c",
-        help="YAML Config File Path to be used for building or running validations.",
+        help="YAML Config File path to be used for building or running validations.",
     )
     run_parser.add_argument(
         "--config-dir",
@@ -610,7 +610,12 @@ def _configure_schema_parser(schema_parser):
     optional_arguments.add_argument(
         "--allow-list",
         "-al",
-        help="Comma separated list of datatype mappings due to incompatible datatypes in source and target. e.g: decimal(12,2):decimal(38,9),string[non-nullable]:string",
+        help="Comma separated list of datatype mappings due to incompatible datatypes in source and target. e.g.: decimal(12,2):decimal(38,9),string[non-nullable]:string,decimal(10-18,0):int64",
+    )
+    optional_arguments.add_argument(
+        "--allow-list-file",
+        "-alf",
+        help="YAML file containing default --allow-list mappings. Can be used in conjunction with --allow-list. e.g.: samples/allow_list/oracle_to_bigquery.yaml or gs://dvt-allow-list-files/oracle_to_bigquery.yaml. See example files in samples/allow_list/",
     )
 
     # Group required arguments
@@ -891,7 +896,7 @@ def _add_common_partition_arguments(optional_arguments, required_arguments):
         "--config-dir",
         "-cdir",
         required=True,
-        help="Directory Path to store YAML Config Files. "
+        help="Directory path to store YAML config files. "
         "GCS: Provide a full gs:// path of the target directory. "
         "Eg: `gs://<BUCKET>/partiitons_dir`. "
         "Local: Provide a relative path of the target directory. "
