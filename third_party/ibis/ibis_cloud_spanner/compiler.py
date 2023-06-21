9dc947d6e0972f8aa384f1056ae9a24344183596
Neha Nene <nehanene@google.com>
2023-06-21 17:24:42 +0000

    tests: spanner integration tests passing, fixes #767 for row validation on timestamp columns

--- third_party/ibis/ibis_cloud_spanner/compiler.py
+++ third_party/ibis/ibis_cloud_spanner/compiler.py
@@ -11,14 +11,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import re
 
 import ibis.expr.operations as ops
 from ibis.backends.base.sql import compiler as sql_compiler
 from ibis.backends.bigquery import compiler as bigquery_compiler
+from ibis.backends.bigquery import rewrites
+from third_party.ibis.ibis_cloud_spanner import registry
 
-compiles = bigquery_compiler.BigQueryExprTranslator.compiles
-rewrites = bigquery_compiler.BigQueryExprTranslator.rewrites
 
+_NAME_REGEX = re.compile(r"[_A-Za-z][A-Za-z_0-9]*")
 
 class SpannerSelect(sql_compiler.Select):
     def format_limit(self):
@@ -28,10 +30,51 @@ class SpannerSelect(sql_compiler.Select):
         limit_sql = f"TABLESAMPLE RESERVOIR ({self.limit.n} ROWS)"
         return limit_sql
 
+class SpannerExprTranslator(sql_compiler.ExprTranslator):
+    """Translate expressions to strings."""
+
+    _registry = registry._registry
+    _rewrites = rewrites.REWRITES
+
+    _forbids_frame_clause = (
+        *sql_compiler.ExprTranslator._forbids_frame_clause,
+        ops.Lag,
+        ops.Lead,
+    )
+
+    _unsupported_reductions = (ops.ApproxMedian, ops.ApproxCountDistinct)
+    _dialect_name = "spanner"
+
+    @staticmethod
+    def _gen_valid_name(name: str) -> str:
+        name = "_".join(_NAME_REGEX.findall(name)) or "tmp"
+        return f"`{name}`"
+
+    def name(self, translated: str, name: str):
+        # replace invalid characters in automatically generated names
+        valid_name = self._gen_valid_name(name)
+        if translated == valid_name:
+            return translated
+        return f"{translated} AS {valid_name}"
+
+    @classmethod
+    def compiles(cls, klass):
+        def decorator(f):
+            cls._registry[klass] = f
+            return f
+
+        return decorator
+
+    def _trans_param(self, op):
+        if op not in self.context.params:
+            raise KeyError(op)
+        return f"@{op.name}"
+
+compiles = SpannerExprTranslator.compiles
 
 class SpannerCompiler(sql_compiler.Compiler):
     # Spanner uses BigQuery SQL syntax
-    translator_class = bigquery_compiler.BigQueryExprTranslator
+    translator_class = SpannerExprTranslator
     table_set_formatter_class = bigquery_compiler.BigQueryTableSetFormatter
     union_class = bigquery_compiler.BigQueryUnion
     intersect_class = bigquery_compiler.BigQueryIntersection
---
+++ third_party/ibis/ibis_cloud_spanner/registry.py
@@ -0,0 +1,60 @@
+# Copyright 2023 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import ibis.expr.operations as ops
+import ibis.expr.datatypes as dt
+from ibis.backends.bigquery.registry import OPERATION_REGISTRY
+
+_registry = OPERATION_REGISTRY.copy()
+
+STRFTIME_FORMAT_FUNCTIONS = {
+    dt.date: "DATE",
+    dt.Timestamp(timezone="UTC"): "TIMESTAMP",
+}
+
+def _compiles_strftime(translator, op):
+    """Timestamp formatting."""
+    arg = op.arg
+    format_str = op.format_str
+    arg_type = arg.output_dtype
+    strftime_format_func_name = STRFTIME_FORMAT_FUNCTIONS[arg_type]
+    fmt_string = translator.translate(format_str)
+    arg_formatted = translator.translate(arg)
+    if isinstance(arg_type, dt.Timestamp) and arg_type.timezone is None:
+        # As per issue-767, fixes issue in Spanner where TIMESTAMP() function
+        # throws exception if column is already a TIMESTAMP
+        arg_formatted = arg_formatted.rstrip(")")
+        arg_formatted = arg_formatted.lstrip("TIMESTAMP(")
+        return "FORMAT_{}({}, {})".format(
+            strftime_format_func_name,
+            fmt_string,
+            arg_formatted,
+        )
+    elif isinstance(arg_type, dt.Timestamp):
+        # As per issue-767, fixes issue in Spanner where TIMESTAMP() function
+        # throws exception if column is already a TIMESTAMP
+        arg_formatted = arg_formatted.rstrip(")")
+        arg_formatted = arg_formatted.lstrip("TIMESTAMP(")
+        return "FORMAT_{}({}, {}, {!r})".format(
+            strftime_format_func_name,
+            fmt_string,
+            arg_formatted,
+            arg_type.timezone,
+        )
+    else:
+        return "FORMAT_{}({}, {})".format(
+            strftime_format_func_name, fmt_string, arg_formatted
+        )
+
+_registry.update({ops.Strftime: _compiles_strftime,})
