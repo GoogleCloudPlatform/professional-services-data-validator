9a343db3d057cf2f416ed99e586dd343fdf2c93d
nj1973 <neiljohnson@google.com>
2024-04-08 09:58:39 +0000

    tests: Add test_row_validation_binary_pk_to_bigquery for Oracle and Snowflake

--- tests/system/data_sources/common_functions.py
+++ tests/system/data_sources/common_functions.py
@@ -12,11 +12,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import string
+from typing import TYPE_CHECKING
+
+from data_validation import __main__ as main
+from data_validation import consts, data_validation
+
+if TYPE_CHECKING:
+    from argparse import Namespace
+    from pandas import DataFrame
+
+
+def binary_key_assertions(df):
+    """Standard assertions for binary key integration test.
+    These tests use BigQuery as a fixed target and execute against all other engines."""
+    # Should be 5 rows in the df all with status success.
+    assert len(df) == 5, "We expect 5 rows with status success from this validation"
+    assert all(
+        _ == consts.VALIDATION_STATUS_SUCCESS for _ in df["validation_status"].to_list()
+    ), "Not all rows have status 'success'"
+    # Validate a sample primary key value is hex(ish).
+    sample_gbc = df["group_by_columns"].to_list().pop()
+    sample_key = [v for _, v in sample_gbc.items()].pop()
+    assert all(_ in string.hexdigits for _ in sample_key)
+
 
 def null_not_null_assertions(df):
     """Standard assertions for null_not_null integration test.
-    These tests use BigQuery as a set target with a mismatch of not null/nullable settings.
-    All other engines are validated against BigQuery to check we get the correct status."""
+    These tests use BigQuery as a fixed target with a mismatch of not null/nullable settings.
+    All other engines are validated against BigQuery to check we get the correct status.
+    """
     # Should be 4 columns in the Dataframe.
     assert len(df) == 4
     match_columns = ["col_nn", "col_nullable"]
@@ -26,10 +51,18 @@ def null_not_null_assertions(df):
         if column_name in match_columns:
             # These columns are the same for all engines and should succeed.
             assert (
-                status == "success"
-            ), f"Column: {column_name}, status: {status} != 'success'"
+                status == consts.VALIDATION_STATUS_SUCCESS
+            ), f"Column: {column_name}, status: {status} != {consts.VALIDATION_STATUS_SUCCESS}"
         elif column_name in mismatch_columns:
             # These columns are the different for source and target engines and should fail.
             assert (
-                status == "fail"
-            ), f"Column: {column_name}, status: {status} != 'fail'"
+                status == consts.VALIDATION_STATUS_FAIL
+            ), f"Column: {column_name}, status: {status} != {consts.VALIDATION_STATUS_FAIL}"
+
+
+def run_test_from_cli_args(args: "Namespace") -> DataFrame:
+    config_managers = main.build_config_managers_from_args(args)
+    assert len(config_managers) == 1
+    config_manager = config_managers[0]
+    validator = data_validation.DataValidation(config_manager.config, verbose=False)
+    return validator.execute()
