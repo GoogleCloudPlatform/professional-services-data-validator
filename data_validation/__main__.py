5a14b4b998eda991dc39a1bf2f3a26bf61edeff0
Neha Nene <nehanene@google.com>
2024-08-15 15:17:54 +0000

    fix: rstrip for custom query row

--- data_validation/__main__.py
+++ data_validation/__main__.py
@@ -254,6 +254,17 @@ def build_config_from_args(args: Namespace, config_manager: ConfigManager):
                 comparison_fields = cli_tools.get_arg_list(
                     args.comparison_fields, default_value=[]
                 )
+
+                # As per #1190, add rstrip for Teradata string comparison fields
+                if (
+                    config_manager.source_client.name == "teradata"
+                    or config_manager.target_client.name == "teradata"
+                ):
+
+                    comparison_fields = config_manager.add_rstrip_to_comp_fields(
+                        comparison_fields
+                    )
+
                 config_manager.append_comparison_fields(
                     config_manager.build_config_comparison_fields(comparison_fields)
                 )
