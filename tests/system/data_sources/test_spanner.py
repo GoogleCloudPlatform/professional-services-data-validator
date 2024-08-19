6b8d2144b7ce050f0cb3528e0b1951e21576180b
nj1973 <neiljohnson@google.com>
2024-08-19 17:17:08 +0000

    test: Add tests for more engines

--- tests/system/data_sources/test_spanner.py
+++ tests/system/data_sources/test_spanner.py
@@ -402,7 +402,7 @@ def test_row_validation_many_columns():
     """Spanner dvt_many_cols row validation.
     This is testing many columns logic for --hash, there's a Teradata test for --concat.
     """
-    row_validation_many_columns_test(schema=None)
+    row_validation_many_columns_test()
 
 
 @mock.patch(
