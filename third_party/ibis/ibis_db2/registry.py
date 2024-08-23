e83ed97680546822ee353e102fba995deb8a6912
helensilva14 <helencristina@google.com>
2024-08-23 21:42:46 +0000

    Add seed to DB2 random function

--- third_party/ibis/ibis_db2/registry.py
+++ third_party/ibis/ibis_db2/registry.py
@@ -448,7 +448,8 @@ def _literal(t, op):
 
 
 def _random(t, expr):
-    return sa.func.random()
+    # https://stackoverflow.com/a/38297161 
+    return sa.func.random(sa.func.setseed(1))
 
 
 def _day_of_week_index(t, op):
