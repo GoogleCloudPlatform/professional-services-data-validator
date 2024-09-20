434032682d7b0fd32e1353d5041da0b14151388f
Nick Redfearn <nick.redfearn@starlingbank.com>
2024-09-20 11:19:01 +0100

    chore: updating based on pr review feedback

--- setup.py
+++ setup.py
@@ -25,31 +25,25 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 dependencies = [
-    "attrs==24.2.0",
-    "Flask==3.0.3",
-    "fsspec==2024.9.0",
-    "google-api-python-client==2.144.0",
-    "google-cloud-bigquery==3.25.0",
-    "google-cloud-bigquery-storage==2.26.0",
-    "google-cloud-secret-manager==2.20.2",
-    "google-cloud-spanner==3.49.1",
-    "google-cloud-storage==2.18.2",
-    "grpcio==1.66.1",
-    "ibis-framework==5.1.0",
-    "impyla==0.19.0",
-    "jellyfish==1.1.0",
-    "lazy-object-proxy==1.10.0",
-    "marshmallow==3.22.0",
-    "pandas==2.0.3",
-    "parsy==2.1",
-    "proto-plus==1.24.0",
-    "psycopg2-binary==2.9.9",
-    "pyarrow==14.0.1", #ibis-framework 7.1.0 depends on pyarrow<15 and >=2
-    "pydata-google-auth==1.8.2",
-    "PyMySQL==1.1.1",
-    "PyYAML==6.0.2",
-    "SQLAlchemy==2.0.34",
-    "tabulate==0.9.0",
+    "Flask<=2.2", # Some versions of airflow such as 2.9.1 depend on flask<2.3 and >=2.2
+    "fsspec>=2024.9.0",
+    "google-api-python-client>=2.144.0",
+    "google-cloud-bigquery>=3.25.0",
+    "google-cloud-bigquery-storage>=2.26.0",
+    "google-cloud-secret-manager>=2.20.2",
+    "google-cloud-spanner>=3.49.1",
+    "google-cloud-storage>=2.18.2",
+    "ibis-framework==5.1.0", # Pinned to 5.1.0, significant work to bump to 7.1.0
+    "impyla>=0.19.0",
+    "jellyfish>=1.1.0",
+    "pandas==2.0.3", # 2.03 is the highest version that still supports python 3.8
+    "parsy>=2.1",
+    "psycopg2-binary>=2.9.9",
+    "pyarrow==14.0.1", # ibis-framework 7.1.0 depends on pyarrow<15 and >=2
+    "pydata-google-auth>=1.8.2",
+    "PyYAML>=6.0.2",
+    "SQLAlchemy>=2.0.34",
+    "tabulate>=0.9.0",
 ]
 
 extras_require = {
