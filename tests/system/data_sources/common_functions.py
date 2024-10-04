b15e3631aadcd6428dc405b6e7ede363d30e9940
helensilva14 <helencristina@google.com>
2024-10-04 22:16:14 +0000

    Add step to Cloud Build YAML

--- tests/system/data_sources/common_functions.py
+++ tests/system/data_sources/common_functions.py
@@ -249,7 +249,6 @@ def column_validation_test_config_managers(
 
 
 def row_validation_test(
-    # pk="id",
     tables="pso_data_validator.test_generate_partitions",
     tc="bq-conn",
     hash="col_int8,col_int16,col_int32,col_int64,col_dec_20,col_dec_38,col_dec_10_2,col_float32,col_float64,col_varchar_30,col_char_2,col_date,col_datetime,col_tstz",
