20db0e4ab8f4087465d69a326109d764d9359f8b
Sundar Mudupalli <106282841+sundar-mudupalli-work@users.noreply.github.com>
2025-05-31 15:58:02 +0000

    Linted

--- tests/system/data_sources/common_functions.py
+++ tests/system/data_sources/common_functions.py
@@ -14,7 +14,7 @@
 
 import json
 import string
-from typing import TYPE_CHECKING, Tuple # Build is still on Python 3.8
+from typing import TYPE_CHECKING, Tuple  # Build is still on Python 3.8
 import pathlib
 
 from data_validation import __main__ as main
