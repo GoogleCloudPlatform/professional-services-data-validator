67f621dc7c7ea09d382a417e59939cc89e1fbfd2
Neil Johnson <neiljohnson@google.com>
2025-06-10 17:38:29 +0100

    Update third_party/ibis/ibis_impala/api.py
    Co-authored-by: Helen Cristina <helensilva14@gmail.com>

--- third_party/ibis/ibis_impala/api.py
+++ third_party/ibis/ibis_impala/api.py
@@ -29,7 +29,7 @@ import fsspec
 import re
 
 
-# Issue 1525 datetime64[ns] overflows datetimes way in the future.
+# Issue-1525: datetime64[ns] overflows datetimes way in the future.
 # Using "object" instead which is what BigQuery implementation results in.
 _DVT_HS2_TTypeId_to_dtype = ibis.backends.impala._HS2_TTypeId_to_dtype.copy()
 _DVT_HS2_TTypeId_to_dtype["TIMESTAMP"] = "object"
