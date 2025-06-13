e885564b5796ebabcd9b36a383aff4d47e5da399
nj1973 <neiljohnson@google.com>
2025-06-13 08:52:59 +0000

    test: Fix some test setup SQL

--- tests/resources/postgresql_test_tables.sql
+++ tests/resources/postgresql_test_tables.sql
@@ -924,7 +924,7 @@ CREATE TABLE pso_data_validator.dvt_tricky_dates (
 COMMENT ON TABLE pso_data_validator.dvt_tricky_dates IS 'Integration test table used to test potentially difficult Timestamps.';
 INSERT INTO pso_data_validator.dvt_tricky_dates VALUES
 (1,DATE'1000-01-01',DATE'1970-01-01',DATE'9999-12-31',DATE'4712-12-31'
-,TIMESTAMP'1000-01-01 00:00:00',TIMESTAMP'1970-01-01 00:00:00',TIMESTAMP'9999-12-31 23:59:59',DATETIME'4712-12-31 23:23:59');
+,TIMESTAMP'1000-01-01 00:00:00',TIMESTAMP'1970-01-01 00:00:00',TIMESTAMP'9999-12-31 23:59:59',TIMESTAMP'4712-12-31 23:23:59');
 INSERT INTO pso_data_validator.dvt_tricky_dates (id) VALUES (2);
 
 DROP TABLE IF EXISTS pso_data_validator.dvt_tricky_strings;
