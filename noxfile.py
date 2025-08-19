46456d5de1f4be4da3ccebf6c6d92cc845137713
nj1973 <neiljohnson@google.com>
2025-08-19 12:17:37 +0000

    test: Experiment with Python 3.12 for tests

--- noxfile.py
+++ noxfile.py
@@ -27,11 +27,12 @@ import random
 import nox
 
 # Python version used for linting.
-DEFAULT_PYTHON_VERSION = "3.10"
+DEFAULT_PYTHON_VERSION = "3.12"
 
 # Python versions used for testing.
-PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11"]
+PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12"]
 PYTHON_VERSIONS_ORACLE = ["3.8", "3.9", "3.10"]
+PYTHON_VERSIONS_ORACLE = PYTHON_VERSIONS
 
 BLACK_PATHS = (
     "data_validation",
