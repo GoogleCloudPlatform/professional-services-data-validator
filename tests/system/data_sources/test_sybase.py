a33de1f23181ae96c96d385bf07b976365870480
nj1973 <neiljohnson@google.com>
2025-10-10 11:51:11 +0000

    feat: Add Sybase support

--- tests/system/data_sources/test_sybase.py
+++ tests/system/data_sources/test_sybase.py
@@ -44,6 +44,7 @@ from tests.system.data_sources.common_functions import (
 )
 
 
+# Our Sybase test infra has a habit of failing to connect but then working on retry.
 pytestmark = pytest.mark.flaky(
     reruns=1,
     reruns_delay=1,
