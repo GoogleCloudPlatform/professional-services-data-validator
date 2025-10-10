75460a30239fa9115fb4a6ed3420a62846dad57c
nj1973 <neiljohnson@google.com>
2025-10-10 11:20:44 +0000

    feat: Add Sybase support

--- tests/system/data_sources/test_sybase.py
+++ tests/system/data_sources/test_sybase.py
@@ -98,7 +98,7 @@ EXPECTED_DATETIME_ID_PARTITION_FILTER = [
 ]
 
 pytestmark = pytest.mark.flaky(
-    reruns=1, reruns_delay=1, only_rerun=["OperationalError"]
+    reruns=1, reruns_delay=1, only_rerun=["DataClientConnectionFailure"]
 )
 # @pytest.mark.flaky(reruns=1, reruns_delay=1, only_rerun=["OperationalError"])
 
