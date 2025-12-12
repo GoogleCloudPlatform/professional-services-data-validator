481441d0415f9cce62d8c0359a96b41832707256
nj1973 <neiljohnson@google.com>
2025-12-12 12:45:20 +0000

    test: Sybase dependencies

--- ci/build.sh
+++ ci/build.sh
@@ -26,7 +26,9 @@ set -eo pipefail
 if [[ "$NOX_SESSION" == "integration_sybase" ]]; then
     sudo add-apt-repository -y universe
     apt-get update
-    sudo apt-get install -y freetds-bin freetds-common freetds-bin
+    sudo apt-get install -y freetds-bin freetds-common
+    # Can't install tdsodbc due to libodbc conflict, but we can download the .so file:
+    sudo apt-get download tdsodbc
     # apt-get remove -y msodbcsql17 libodbc2
     # apt autoremove -y
     # apt-get install -y tdsodbc
