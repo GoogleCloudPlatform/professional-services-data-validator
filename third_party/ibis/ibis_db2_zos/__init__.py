36906d77625f99960e1d4750c6867880cfbea104
nj1973 <neiljohnson@google.com>
2025-12-17 15:42:56 +0000

    feat: Prototype Db2 z/OS support

---
+++ third_party/ibis/ibis_db2_zos/__init__.py
@@ -0,0 +1,67 @@
+# Copyright 2025 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Optional, Dict, Any
+
+import sqlalchemy as sa
+
+from third_party.ibis.ibis_db2 import Backend as Db2LUWBackend
+
+
+class Backend(Db2LUWBackend):
+    name = "db2_zos"
+
+    def do_connect(
+        self,
+        host: str = "localhost",
+        user: Optional[str] = None,
+        password: Optional[str] = None,
+        port: int = 50000,
+        database: Optional[str] = None,
+        url: Optional[str] = None,
+        driver: str = "ibm_db_sa",
+        connect_args: Dict[str, Any] = None,
+    ) -> None:
+        if url is None:
+            if driver != "ibm_db_sa":
+                raise NotImplementedError(
+                    "ibm_db_sa is currently the only supported driver"
+                )
+            sa_url = sa.engine.url.URL.create(
+                "ibm_db_sa",
+                host=host,
+                port=port,
+                username=user,
+                password=password,
+                database=database,
+            )
+        else:
+            sa_url = sa.engine.url.make_url(url)
+
+        engine = sa.create_engine(
+            sa_url,
+            poolclass=sa.pool.StaticPool,
+            # Pessimistic disconnect handling
+            pool_pre_ping=True,
+            connect_args=connect_args or {},
+        )
+        self.database_name = database
+        self.url = sa_url
+
+        @sa.event.listens_for(engine, "connect")
+        def connect(dbapi_connection, connection_record):
+            with dbapi_connection.cursor() as cur:
+                cur.execute("SET SESSION TIME ZONE = '+0:00'")
+
+        super(Db2LUWBackend).do_connect(engine)
---
+++ third_party/ibis/ibis_db2_zos/api.py
@@ -0,0 +1,45 @@
+# Copyright 2025 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Optional
+
+from third_party.ibis.ibis_db2_zos import Backend
+import ibm_db_sa  # NOQA fail early if driver is missing
+
+from data_validation.util import dvt_config_string_to_dict
+
+
+def db2_connect(
+    host: str = "localhost",
+    user: Optional[str] = None,
+    password: Optional[str] = None,
+    port: int = 50000,
+    database: Optional[str] = None,
+    url: Optional[str] = None,
+    driver: str = "ibm_db_sa",
+    connect_args: Optional[str] = None,
+):
+    connect_args_dict = dvt_config_string_to_dict(connect_args) if connect_args else {}
+    backend = Backend()
+    backend.do_connect(
+        host=host,
+        user=user,
+        password=password,
+        port=port,
+        database=database,
+        url=url,
+        driver=driver,
+        connect_args=connect_args_dict,
+    )
+    return backend
