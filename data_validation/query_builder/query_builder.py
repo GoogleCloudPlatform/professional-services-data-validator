c46b5a04e269f855afd5b6863f389f7597c08b06
nj1973 <neiljohnson@google.com>
2026-01-30 13:33:18 +0000

    chore: Remove changes that were instead moved to issue 1661

--- data_validation/query_builder/query_builder.py
+++ data_validation/query_builder/query_builder.py
@@ -341,10 +341,7 @@ class CalculatedField(object):
     @staticmethod
     def ifnull(config, fields):
         default_null_string = ibis.literal(
-            config.get(
-                consts.CALC_FIELD_IFNULL_DEFAULT,
-                consts.CALC_FIELD_IFNULL_DEFAULT_STRING,
-            )
+            config.get("default_null_string", "DEFAULT_REPLACEMENT_STRING")
         )
         fields = [fields[0], default_null_string]
         return CalculatedField(
