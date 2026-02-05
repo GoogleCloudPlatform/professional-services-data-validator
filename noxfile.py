43ec67b0328a86900d168db1fd9511983cbb802d
nj1973 <neiljohnson@google.com>
2026-02-05 18:19:36 +0000

    chore: Temporarily change test Python to 3.11

--- noxfile.py
+++ noxfile.py
@@ -27,12 +27,12 @@ import random
 import nox
 
 # Python version used for linting.
-DEFAULT_PYTHON_VERSION = "3.10"
+DEFAULT_PYTHON_VERSION = "3.11"
 
 # Python versions used for testing.
-#PYTHON_VERSIONS = ["3.9", "3.10", "3.11"]
+#PYTHON_VERSIONS = ["3.10", "3.11"]
 PYTHON_VERSIONS = ["3.11"]
-#PYTHON_VERSIONS_ORACLE = ["3.9", "3.10"]
+#PYTHON_VERSIONS_ORACLE = ["3.10"]
 PYTHON_VERSIONS_ORACLE = ["3.11"]
 
 BLACK_PATHS = (
