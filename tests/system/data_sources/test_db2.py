6a55378ba33c1abdae0d18505008dc4aef4ea5c2
nj1973 <neiljohnson@google.com>
2026-02-06 18:32:32 +0000

    feat: Prototype Db2 z/OS support

--- tests/system/data_sources/test_db2.py
+++ tests/system/data_sources/test_db2.py
@@ -490,6 +490,18 @@ def test_varchar_pk_query_row_validation_to_bigquery():
     "data_validation.state_manager.StateManager.get_connection_config",
     new=mock_get_connection_config,
 )
+def test_fixed_char_pk_row_validation_to_bigquery():
+    """Test fixed char primary keys"""
+    id_column_row_validation_test(
+        "db2inst1.dvt_fixed_char_id=pso_data_validator.dvt_fixed_char_id",
+        use_random_row=False,
+    )
+
+
+@mock.patch(
+    "data_validation.state_manager.StateManager.get_connection_config",
+    new=mock_get_connection_config,
+)
 def test_find_tables():
     """Oracle to BigQuery test of find-tables command."""
     find_tables_test()
