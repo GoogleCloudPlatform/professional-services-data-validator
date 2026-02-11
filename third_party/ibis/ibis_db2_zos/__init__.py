284ff31ef60d7185ebd6b9ccdeafd2e8a06b5334
nj1973 <neiljohnson@google.com>
2026-02-11 15:50:32 +0000

    feat: Prototype Db2 z/OS support

--- third_party/ibis/ibis_db2_zos/__init__.py
+++ third_party/ibis/ibis_db2_zos/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Iterable, Optional, Dict, Any
+from typing import Iterable, Optional, Dict, Any, Tuple
 
 import sqlalchemy as sa
 
