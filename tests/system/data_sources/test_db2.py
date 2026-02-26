cf42db9f564b7f480107fe9c7ec171515742361a
nj1973 <neiljohnson@google.com>
2026-02-26 08:56:09 +0000

    test: Add tests for binary columns

--- tests/system/data_sources/test_db2.py
+++ tests/system/data_sources/test_db2.py
@@ -191,6 +191,7 @@ def test_column_validation_db2_types_to_bigquery():
     """Db2 to BigQuery dvt_db2_types column validation"""
     cols = "*"
     # TODO Add col_char_bit and col_varchar_bit into cols below once issue-1655 is resolved.
+    # TODO Add col_binary and col_varbinary into cols below once issue-1354 is resolved.
     cols = (
         "col_smallint,col_int,col_bigint,col_decfloat_16,col_decfloat_32,col_clob,col_nvarchar_30,col_nchar_2,"
         "col_nclob,col_dbclob,col_blob,col_graphic,col_vargraphic,col_xml"
@@ -354,6 +355,7 @@ def test_row_validation_db2_types_to_bigquery():
     # Excluded col_clob,col_nclob,col_xml because they are incompatible with hex() function (due to potential length).
     # TODO Add col_char_2 to list below once issue-1354 is complete.
     # TODO Add col_char_bit,col_varchar_bit to list below once issue-1655 is complete.
+    # TODO Add col_binary and col_varbinary into cols below once issue-1354 is resolved.
     cols = "col_smallint,col_int,col_bigint,col_dec_10_2,col_decfloat_16,col_decfloat_32,col_nvarchar_30,col_time,col_blob"
     row_validation_test(
         tables="pso_data_validator.dvt_db2_types",
