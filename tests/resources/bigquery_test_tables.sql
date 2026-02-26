1c9ca435f5c933e09bd885ecebebda6cfb40f58b
nj1973 <neiljohnson@google.com>
2026-02-26 08:52:45 +0000

    test: Add tests for binary columns

--- tests/resources/bigquery_test_tables.sql
+++ tests/resources/bigquery_test_tables.sql
@@ -226,18 +226,16 @@ CREATE OR REPLACE TABLE pso_data_validator.dvt_db2_types
 ,   col_nclob       STRING
 ,   col_dbclob      STRING
 ,   col_blob        BYTES
+-- TODO Switch the two "_bit" columns to BYTES after issue-1655 is complete.
 ,   col_char_bit    STRING
 ,   col_varchar_bit STRING
---,   col_char_bit    BYTES
---,   col_varchar_bit BYTES
 ,   col_graphic     STRING
 ,   col_vargraphic  STRING
 ,   col_date        DATE
 ,   col_timestamp   DATETIME
 ,   col_time        TIME
--- TODO Binary columns to be added after issue-1354 is merged.
---,   col_binary      BYTES
---,   col_varbinary   BYTES
+,   col_binary      BYTES
+,   col_varbinary   BYTES
 ,   col_xml         STRING
 ) OPTIONS (description='Db2 data types integration test table (BigQuery target)');
 
@@ -245,26 +243,25 @@ INSERT INTO pso_data_validator.dvt_db2_types VALUES
 (1,123,12345,1123456789,1.1,123.456,123456.789
 ,'Hello CLOB','Hello NVARCHAR','A ','Hello NCLOB','Hello DBCLOB',CAST('Hello BLOB' AS BYTES)
 ,'ABC','DEF'
--- TODO Replace ABC,DEF line with UUID hex line below once issue-1655 is complete.
+-- TODO Replace ABC,DEF line above with UUID hex line below once issue-1655 is complete.
 --,FROM_HEX('550E8400E29B41D4A716446655440000'),FROM_HEX('550E8400E29B41D4A716446655440000')
 ,'GHI','JKL'
 ,DATE'1970-01-01',DATETIME'1970-01-01 00:00:01.123456',TIME'00:00:01'
---,CAST('A' AS BYTES),CAST('A' AS BYTES)
+,CAST('A' AS BYTES),CAST('A' AS BYTES)
 ,'<xml></xml>'),
 (2,123,12345,1123456789,0,123.456,123456.789
 ,'Hello CLOB2','Hello NVARCHAR2','B ','Hello NCLOB2','Hello DBCLOB2',CAST('Hello BLOB2' AS BYTES)
 ,'ABC','DEF'
--- TODO Replace ABC,DEF line with UUID hex line below once issue-1655 is complete.
+-- TODO Replace ABC,DEF line above with UUID hex line below once issue-1655 is complete.
 --,FROM_HEX('F2A79E538CBD4A1E9F03B8D4C731A9F4'),FROM_HEX('F2A79E538CBD4A1E9F03B8D4C731A9F4')
 ,'GHI','JKL'
 ,DATE'1970-01-02',DATETIME'1970-01-02 00:00:02.001',TIME'00:00:02'
---,CAST('B' AS BYTES),CAST('B' AS BYTES)
+,CAST('B' AS BYTES),CAST('B' AS BYTES)
 ,'<xml></xml>'),
 (3,NULL,NULL,NULL,NULL,NULL,NULL
-,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL
-,NULL,NULL,NULL,NULL,NULL
---,NULL,NULL
-,NULL);
+,NULL,NULL,NULL,NULL,NULL,NULL,NULL
+,NULL,NULL,NULL,NULL,NULL,NULL
+,NULL,NULL,NULL);
 
 CREATE OR REPLACE TABLE `pso_data_validator`.`dvt_binary`
 (   binary_id       BYTES(16) NOT NULL
