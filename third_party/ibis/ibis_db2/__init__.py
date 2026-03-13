dca1f712d8f9c96d0678d3edf3c620df6435b402
nj1973 <neiljohnson@google.com>
2026-03-13 11:07:27 +0000

    feat: Add Db2 z/OS support

--- third_party/ibis/ibis_db2/__init__.py
+++ third_party/ibis/ibis_db2/__init__.py
@@ -85,12 +85,6 @@ class Backend(BaseAlchemyBackend):
         self.database_name = database
         self.url = sa_url
 
-        # We cannot find a SET TIMEZONE command for LUW therefore commented this section out.
-        # @sa.event.listens_for(engine, "connect")
-        # def connect(dbapi_connection, connection_record):
-        #    with dbapi_connection.cursor() as cur:
-        #        cur.execute("SET TIMEZONE = 'UTC'")
-
         super().do_connect(engine)
 
     def find_db(self):
