b62e238bb63104588a471b3e1e56e6b1c09bae26
Neil Johnson <neiljohnson@google.com>
2026-05-05 15:18:10 +0100

    fix: Review and update samples/cloud_run files (#1730)
    * fix: Review and update samples/cloud_run files
    
    * docs: Update Python version in sample Dockerfile
    
    * docs: Add sample Dockerfile with Oracle client commands
    
    * fix: Update data_validation/app.py to match samples/cloud_run/main.py
    
    * chore: Remove unused import

--- data_validation/app.py
+++ data_validation/app.py
@@ -12,24 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
+import logging
 import os
 from data_validation import data_validation
 import flask
-import pandas
-import logging
 
 app = flask.Flask(__name__)
 
 
 def _clean_dataframe(df):
-    rows = df.to_dict(orient="record")
-    for row in rows:
-        for key in row:
-            if type(row[key]) in [pandas.Timestamp]:
-                row[key] = str(row[key])
-
-    return json.dumps(rows)
+    return df.to_json(orient="records", date_format="iso")
 
 
 def _get_request_content(request):
@@ -58,13 +50,19 @@ def main(request):
 
 @app.route("/", methods=["POST"])
 def run():
+    """Handle incoming Data Validation requests.
+
+    request (flask.Request): HTTP request object.
+    """
     try:
         config = _get_request_content(flask.request)
         result = validate(config)
-        return str(result)
-    except Exception as e:
-        logging.exception(e)
-        return "Found Error: {}".format(e)
+        return flask.Response(result, mimetype="application/json")
+    except Exception:
+        logging.exception("An error occurred during validation")
+        return flask.Response(
+            "An internal server error occurred.", status=500, mimetype="text/plain"
+        )
 
 
 @app.route("/test", methods=["POST"])
