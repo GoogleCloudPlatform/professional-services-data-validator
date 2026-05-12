183d6078084f41721127e8c683b21d29a9362306
nj1973 <neiljohnson@google.com>
2026-05-12 15:02:11 +0000

    fix: Fix circular import

--- data_validation/util.py
+++ data_validation/util.py
@@ -18,7 +18,7 @@ import re
 import time
 import uuid
 
-from data_validation import clients, exceptions
+from data_validation import exceptions
 
 from typing import TYPE_CHECKING
 
@@ -72,6 +72,7 @@ def dvt_config_string_to_dict(config_string: str) -> dict:
 
 
 def ibis_table_to_sql(ibis_table: "IbisTable", alchemy_client: "BaseBackend") -> str:
+    from data_validation import clients
     """Function to generate the SQL string for the table based on the backend.
 
     We need the client in order to find the dialect, otherwise we end up with generic literals.
