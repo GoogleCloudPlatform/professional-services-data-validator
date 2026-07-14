01275e24e9b41869dc5af42158036e48c5b6bb91
nj1973 <neiljohnson@google.com>
2026-07-14 12:22:28 +0000

    test: Add BigQuery custom endpoint unit test

--- tests/unit/ibis_bigquery/test_endpoints.py
+++ tests/unit/ibis_bigquery/test_endpoints.py
@@ -19,14 +19,15 @@ from google.auth import credentials
 
 from data_validation import clients, consts
 
+
 @mock.patch("google.auth.default")
 def test_bigquery_custom_endpoints(mock_default):
     mock_credentials = mock.create_autospec(credentials.Credentials)
     mock_default.return_value = (mock_credentials, "test-project")
-    
+
     api_endpoint = "https://bigquery-custom.googleapis.com"
     storage_api_endpoint = "https://bigquerystorage-custom.googleapis.com"
-    
+
     conn_config = {
         consts.SOURCE_TYPE: consts.SOURCE_TYPE_BIGQUERY,
         consts.PROJECT_ID: "test-project",
@@ -35,6 +36,6 @@ def test_bigquery_custom_endpoints(mock_default):
     }
 
     ibis_client = clients.get_data_client(conn_config)
-    
+
     assert ibis_client.client._connection.API_BASE_URL == api_endpoint
     assert ibis_client.storage_client.api_endpoint == storage_api_endpoint
