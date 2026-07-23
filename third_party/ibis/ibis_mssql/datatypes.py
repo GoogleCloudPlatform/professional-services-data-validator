45ba1a08486fd033b8e95ca68889fd9de407448e
Sundar Mudupalli <mudupalli@google.com>
2026-07-23 23:27:04 +0000

    Linted

--- third_party/ibis/ibis_mssql/datatypes.py
+++ third_party/ibis/ibis_mssql/datatypes.py
@@ -34,6 +34,7 @@ def sa_mssql_datetime2(_, sa_type, nullable=True):
 _MSSQL_TYPE_MAP[dt.String] = mssql.VARCHAR
 _MSSQL_TYPE_MAP[dt.Float64] = mssql.FLOAT
 
+
 @dt.dtype.register(MSDialect, mssql.TINYINT)
 def sa_mssql_tinyint(_, sa_type, nullable=True):
     return dt.Int16(nullable=nullable)
