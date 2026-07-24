06d886a5a76184a108846f61e60eb267fe254fdd
nj1973 <neiljohnson@google.com>
2026-07-24 10:21:56 +0000

    test: Experiment with Python 3.12/13

--- noxfile.py
+++ noxfile.py
@@ -32,6 +32,10 @@ DEFAULT_PYTHON_VERSION = "3.11"
 # Python versions used for testing.
 PYTHON_VERSIONS = ["3.10", "3.11"]
 
+# Temporary for experimentation.
+PYTHON_VERSIONS = ["3.12", "3.13"]
+DEFAULT_PYTHON_VERSION = "3.12"
+
 BLACK_PATHS = (
     "data_validation",
     "samples",
