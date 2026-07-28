b4eb1cbc36eab1ae6229c0c24d147aed1e95bb11
Neil Johnson <neiljohnson@google.com>
2026-07-28 08:12:21 +0100

    fix: Fix SQL Server overflow on SUM(INT) (#1790)
    * test: Add test for dvt_int_overflow on SQL Server
    
    * test: Add test for dvt_int_overflow on Sybase
    
    * fix: Fix SQL Server overflow on SUM(INT)
    
    * fix: Fix Sybase overflow on SUM(INT)

--- third_party/ibis/ibis_addon/operations.py
+++ third_party/ibis/ibis_addon/operations.py
@@ -495,6 +495,7 @@ MsSqlExprTranslator._registry[ops.TableColumn] = mssql_registry.sa_table_column
 MsSqlExprTranslator._registry[ops.ExtractEpochSeconds] = mssql_registry.sa_epoch_seconds
 MsSqlExprTranslator._registry[ops.RStrip] = mssql_registry.sa_whitespace_rstrip
 MsSqlExprTranslator._registry[ops.Mean] = mssql_registry.sa_format_mean
+MsSqlExprTranslator._registry[ops.Sum] = mssql_registry.sa_format_sum
 MsSqlExprTranslator._registry[PaddedCharLength] = MsSqlExprTranslator._registry[
     ops.StringLength
 ]
@@ -557,3 +558,5 @@ if SybaseExprTranslator:
     SybaseExprTranslator._registry[PaddedCharLength] = (
         mssql_registry.sa_format_string_length
     )
+    SybaseExprTranslator._registry[ops.Mean] = mssql_registry.sa_format_mean
+    SybaseExprTranslator._registry[ops.Sum] = mssql_registry.sa_format_sum
