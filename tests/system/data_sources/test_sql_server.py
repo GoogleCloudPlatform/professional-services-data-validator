78658ab718ca021e8a280f1a4c649a8c39ab834e
Neil Johnson <neiljohnson@google.com>
2026-07-28 08:09:05 +0100

    fix: SQL Server tinyint type detection (#1784)
    * fix: SQL Server tinyiny type detection
    
    * Linted
    
    * test: Remove test workarounds since reinstating link to sql_server datatypes.py
    
    ---------
    
    Co-authored-by: Sundar Mudupalli <mudupalli@google.com>

--- tests/system/data_sources/test_sql_server.py
+++ tests/system/data_sources/test_sql_server.py
@@ -335,9 +335,7 @@ def test_schema_validation_core_types_to_bigquery():
             # All SQL Server integers go to BigQuery INT64.
             "int8:int64,int16:int64,int32:int64,!int32:!int64,"
             # BigQuery does not have a float32 type.
-            "float32:float64,"
-            # SQL Server TIMESTAMP type has scale=7 on Ibis which does not happen in BigQuery.
-            "timestamp(7):timestamp,!timestamp(7):!timestamp,timestamp(7, 'UTC'):timestamp('UTC'),"
+            "float32:float64"
         ),
     )
 
@@ -355,9 +353,7 @@ def test_schema_validation_ss_types_to_bigquery():
             # All SQL Server integers go to BigQuery INT64.
             "int8:int64,int16:int64,int32:int64,!int32:!int64,"
             # BigQuery does not have a float32 type.
-            "float32:float64,"
-            # SQL Server datetime scales are picked up.
-            "timestamp(7):timestamp,timestamp(7, 'UTC'):timestamp('UTC')"
+            "float32:float64"
         ),
         # TODO money types are being identified as integers - issue-1582
         exclusion_columns="col_money,col_smallmoney",
@@ -378,8 +374,6 @@ def test_schema_validation_not_null_vs_nullable():
             "-sc=sql-conn",
             "-tc=bq-conn",
             "-tbls=pso_data_validator.dvt_null_not_null=pso_data_validator.dvt_null_not_null",
-            # SQL Server TIMESTAMP type has scale=7 on Ibis which does not happen in BigQuery.
-            "--allow-list=timestamp(7):timestamp,!timestamp(7):!timestamp,timestamp(7, 'UTC'):timestamp('UTC'),",
         ]
     )
     df = run_test_from_cli_args(args)
@@ -501,8 +495,6 @@ def test_column_validation_ss_types_to_bigquery():
             "col_ntext",
         )
     ]
-    # TODO Include col_int1 in max_cols when working on issue-1585.
-    max_cols = [_ for _ in cols if _ not in ("col_int1",)]
     avg_cols = [
         _
         for _ in cols
@@ -514,7 +506,7 @@ def test_column_validation_ss_types_to_bigquery():
         count_cols=",".join(cols),
         sum_cols=",".join(cols),
         min_cols=",".join(cols),
-        max_cols=",".join(max_cols),
+        max_cols=",".join(cols),
         avg_cols=",".join(avg_cols),
         # SQL Server requires cast_to_bigint for summing tinyint/smallint/int.
         cast_to_bigint=True,
@@ -707,8 +699,6 @@ def test_row_validation_comp_fields_ss_types_to_bigquery():
                 "id",
                 # Excluded col_float32 because BigQuery does not have a float32 type.
                 "col_float32",
-                # TODO Include col_int1 in max_cols when working on issue-1585.
-                "col_int1",
                 # TODO Remove money types from exclude list when working on - issue-1582
                 "col_money",
                 "col_smallmoney",
